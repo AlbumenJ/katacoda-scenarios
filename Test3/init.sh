abc2891a12b8045a3ae1c9301275f6e8dfd2a862
Albumen <jhq0812@gmail.com>
2020-08-20 23:26:44 +0800

    update sh

--- Test3/init.sh
+++ Test3/init.sh
@@ -6,17 +6,16 @@ sleep 10
 
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 
-helm install stable/docker-registry \
-  --name registry \
+helm install registry stable/docker-registry \
   --version 1.9.4 \
   --namespace kube-system \
-  --set service.port=4567
+  --set service.port=4567 \
   --set service.type=ClusterIP \
   --set service.clusterIP=10.98.0.10
   
 nohup kubectl port-forward --namespace kube-system \
 $(kubectl get po -n kube-system | grep registry-docker-registry | \
-awk '{print $1;}') 5000:5000 >/dev/null 2>&1 &
+awk '{print $1;}') 5000:4567 >/dev/null 2>&1 &
 
 echo "10.98.0.10 registry.test.training.katacoda.com" >> /etc/hosts
 
